5110bfe9cf66150e992c469f65d3412bcbb9162c
tenbeitelh <tenbeitelholger@googlemail.com>
2016-01-05 20:06:03 +0100

    t

--- Mahout/clustering/RunKMeans/src/de/hs/osnabrueck/tenbeitel/mahout/RunKMeans.java
+++ Mahout/clustering/RunKMeans/src/de/hs/osnabrueck/tenbeitel/mahout/RunKMeans.java
@@ -56,7 +56,7 @@ public class RunKMeans extends Configured implements Tool {
 
 		GermanAnalyzer analyzer = new GermanAnalyzer(Version.LUCENE_46);
 
-		DocumentProcessor.tokenizeDocuments(new Path(inputDir), analyzer.getClass().asSubclass(GermanAnalyzer.class),
+		CustomDocumentProcessor.tokenizeDocuments(new Path(inputDir), analyzer.getClass().asSubclass(GermanAnalyzer.class),
 				tokenizedPath, conf);
 
 		String tfDirName = "tf-vectors";
