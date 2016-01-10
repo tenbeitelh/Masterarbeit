a82e59d757c099f4d55192cb5970692a6045c1b6
tenbeitelh <tenbeitelholger@googlemail.com>
2016-01-10 17:15:55 +0100

    MR bug fixing

--- MapReduce/src/de/hs/osnabrueck/tenbeitel/mr/informationflow/InformationflowMapper.java
+++ MapReduce/src/de/hs/osnabrueck/tenbeitel/mr/informationflow/InformationflowMapper.java
@@ -2,12 +2,13 @@ package de.hs.osnabrueck.tenbeitel.mr.informationflow;
 
 import java.io.IOException;
 
+import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class InformationflowMapper extends Mapper<Object, Object, Object, Object> {
+public class InformationflowMapper extends Mapper<Writable, Writable, Writable, Writable> {
 
 	@Override
-	protected void map(Object key, Object value, Context context) throws IOException, InterruptedException {
+	protected void map(Writable key, Writable value, Context context) throws IOException, InterruptedException {
 		System.out.println("KEY[class:[" + key.getClass().getName() + "]]:[" + key.toString() + "]VALUE[class:"
 				+ value.getClass().getName() + "]:[" + value.toString() + "]");
 	}
--- MapReduce/src/de/hs/osnabrueck/tenbeitel/mr/informationflow/InformationflowReducer.java
+++ MapReduce/src/de/hs/osnabrueck/tenbeitel/mr/informationflow/InformationflowReducer.java
@@ -2,9 +2,10 @@ package de.hs.osnabrueck.tenbeitel.mr.informationflow;
 
 import java.io.IOException;
 
+import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class InformationflowReducer extends Reducer<Object, Object, Object, Object> {
+public class InformationflowReducer extends Reducer<Writable, Writable, Writable, Writable> {
 
 	@Override
 	public void run(Context context) throws IOException, InterruptedException {
