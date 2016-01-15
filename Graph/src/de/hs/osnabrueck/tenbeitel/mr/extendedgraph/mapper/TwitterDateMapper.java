2f4b290d68aa63de86fd043114ed8612f85d7ee8
tenbeitelh <tenbeitelholger@googlemail.com>
2016-01-15 12:44:50 +0100

    changes to get a empty vector

--- Graph/src/de/hs/osnabrueck/tenbeitel/mr/extendedgraph/mapper/TwitterDateMapper.java
+++ Graph/src/de/hs/osnabrueck/tenbeitel/mr/extendedgraph/mapper/TwitterDateMapper.java
@@ -1,10 +1,14 @@
 package de.hs.osnabrueck.tenbeitel.mr.extendedgraph.mapper;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.mahout.clustering.classify.WeightedPropertyVectorWritable;
+import org.apache.mahout.math.NamedVector;
+import org.apache.mahout.math.SequentialAccessSparseVector;
 
 import de.hs.osnabrueck.tenbeitel.mr.extendgraph.io.DateVectorWritable;
 import de.hs.osnabrueck.tenbeitel.mr.extendgraph.io.TextPair;
@@ -16,7 +20,11 @@ public class TwitterDateMapper extends Mapper<Text, Text, TextPair, DateVectorWr
 	@Override
 	protected void map(Text key, Text value, Context context) throws IOException, InterruptedException {
 		keyPair = new TextPair(key, new Text(String.valueOf(Integer.MAX_VALUE)));
-		dateVector = new DateVectorWritable(value, new WeightedPropertyVectorWritable());
+
+		NamedVector vector = new NamedVector(new SequentialAccessSparseVector(0), "N/A");
+		Map<Text, Text> properties = new HashMap<Text, Text>();
+		WeightedPropertyVectorWritable writableVector = new WeightedPropertyVectorWritable(0, vector, properties);
+		dateVector = new DateVectorWritable(value, writableVector);
 
 		context.write(keyPair, dateVector);
 	}
