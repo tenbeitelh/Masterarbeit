4f692a29d52f2b8fa72f1240cf8d310087ecfaeb
tenbeitelh <tenbeitelholger@googlemail.com>
2016-01-16 17:42:29 +0100

    generic version of sequencefile reader

--- Utilities/SequenceFileReader/src/de/hs/osnabrueck/tenbeitel/utilities/SequenceFileReader.java
+++ Utilities/SequenceFileReader/src/de/hs/osnabrueck/tenbeitel/utilities/SequenceFileReader.java
@@ -11,6 +11,8 @@ import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.util.ReflectionUtils;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
+import org.apache.mahout.clustering.classify.WeightedPropertyVectorWritable;
+import org.apache.mahout.math.NamedVector;
 //import org.apache.mahout.clustering.classify.WeightedPropertyVectorWritable;
 //import org.apache.mahout.math.NamedVector;
 
@@ -31,34 +33,29 @@ public class SequenceFileReader extends Configured implements Tool {
 	public int run(String[] args) throws IOException {
 		Configuration conf = this.getConf();
 
-		String clusterOutput = args[0];
-
-		// Option filePath = SequenceFile.Reader.file(new Path(clusterOutput +
-		// "/clusteredPoints" + "/part-m-00000"));
-		Option filePath = SequenceFile.Reader.file(new Path(clusterOutput));
+		Option filePath = SequenceFile.Reader.file(new Path(args[0]));
 		try (SequenceFile.Reader reader = new SequenceFile.Reader(conf, filePath)) {
 
 			Writable key = (Writable) ReflectionUtils.newInstance(reader.getKeyClass(), conf);
 			Writable value = (Writable) ReflectionUtils.newInstance(reader.getValueClass(), conf);
 
-			// System.out.println(reader.getKeyClassName());
-			// System.out.println(reader.getValueClassName());
+			if (key instanceof WeightedPropertyVectorWritable) {
+				System.out.println("twitter_id" + LINE_DELIMITER + "cluster_id");
+				while (reader.next(key, value)) {
+					NamedVector nVector = (NamedVector) ((WeightedPropertyVectorWritable) value).getVector();
+					System.out.println(nVector.getName() + " belongs to cluster " + key.toString());
+				}
+			} else {
 
-			System.out.println("twitter_id" + LINE_DELIMITER + "cluster_id");
-			int recodCounter = 0;
-			while (reader.next(key, value)) {
+				int recodCounter = 0;
+				while (reader.next(key, value)) {
 
-				recodCounter++;
-				System.out.println(key.toString() + "\t" + value.toString());
-				// NamedVector nVector = (NamedVector)
-				// ((WeightedPropertyVectorWritable) value).getVector();
-				// System.out.println(nVector.getName() + " belongs to cluster "
-				// + key.toString());
-				// System.out.println(nVector.getDelegate().getClass());
-				// System.out.println(nVector.getName() + LINE_DELIMITER +
-				// key.toString());
+					recodCounter++;
+					System.out.println(key.toString() + "\t" + value.toString());
+
+				}
+				System.out.println("Founrd " + recodCounter + "similar twitter messages");
 			}
-			System.out.println("Founrd " + recodCounter + "similar twitter messages");
 		}
 
 		return 1;
