f0551991a28a3d38f3974e63a284581a78c68dc5
tenbeitelh <tenbeitelholger@gmx.de>
2016-01-16 17:36:55 +0100

    changed sequencefile reader for similar ids

--- Utilities/SequenceFileReader/src/de/hs/osnabrueck/tenbeitel/utilities/SequenceFileReader.java
+++ Utilities/SequenceFileReader/src/de/hs/osnabrueck/tenbeitel/utilities/SequenceFileReader.java
@@ -1,8 +1,6 @@
 package de.hs.osnabrueck.tenbeitel.utilities;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
@@ -10,12 +8,11 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.SequenceFile;
 import org.apache.hadoop.io.SequenceFile.Reader.Option;
 import org.apache.hadoop.io.Writable;
-import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.util.ReflectionUtils;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
-import org.apache.mahout.clustering.classify.WeightedPropertyVectorWritable;
-import org.apache.mahout.math.NamedVector;
+//import org.apache.mahout.clustering.classify.WeightedPropertyVectorWritable;
+//import org.apache.mahout.math.NamedVector;
 
 public class SequenceFileReader extends Configured implements Tool {
 	private static final String LINE_DELIMITER = "\t";
@@ -36,7 +33,9 @@ public class SequenceFileReader extends Configured implements Tool {
 
 		String clusterOutput = args[0];
 
-		Option filePath = SequenceFile.Reader.file(new Path(clusterOutput + "/clusteredPoints" + "/part-m-00000"));
+		// Option filePath = SequenceFile.Reader.file(new Path(clusterOutput +
+		// "/clusteredPoints" + "/part-m-00000"));
+		Option filePath = SequenceFile.Reader.file(new Path(clusterOutput));
 		try (SequenceFile.Reader reader = new SequenceFile.Reader(conf, filePath)) {
 
 			Writable key = (Writable) ReflectionUtils.newInstance(reader.getKeyClass(), conf);
@@ -46,13 +45,20 @@ public class SequenceFileReader extends Configured implements Tool {
 			// System.out.println(reader.getValueClassName());
 
 			System.out.println("twitter_id" + LINE_DELIMITER + "cluster_id");
+			int recodCounter = 0;
 			while (reader.next(key, value)) {
-				NamedVector nVector = (NamedVector) ((WeightedPropertyVectorWritable) value).getVector();
+
+				recodCounter++;
+				System.out.println(key.toString() + "\t" + value.toString());
+				// NamedVector nVector = (NamedVector)
+				// ((WeightedPropertyVectorWritable) value).getVector();
 				// System.out.println(nVector.getName() + " belongs to cluster "
 				// + key.toString());
-				System.out.println(nVector.getDelegate().getClass());
-				System.out.println(nVector.getName() + LINE_DELIMITER + key.toString());
+				// System.out.println(nVector.getDelegate().getClass());
+				// System.out.println(nVector.getName() + LINE_DELIMITER +
+				// key.toString());
 			}
+			System.out.println("Founrd " + recodCounter + "similar twitter messages");
 		}
 
 		return 1;
