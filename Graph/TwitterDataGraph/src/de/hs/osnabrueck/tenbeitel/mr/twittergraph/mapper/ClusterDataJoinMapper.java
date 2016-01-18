02934d0ea19246f99326d8db7e01a578424d0748
tenbeitelh <tenbeitelholger@googlemail.com>
2016-01-18 21:24:12 +0100

    empty mapper

--- Graph/TwitterDataGraph/src/de/hs/osnabrueck/tenbeitel/mr/twittergraph/mapper/ClusterDataJoinMapper.java
+++ Graph/TwitterDataGraph/src/de/hs/osnabrueck/tenbeitel/mr/twittergraph/mapper/ClusterDataJoinMapper.java
@@ -1,5 +1,7 @@
 package de.hs.osnabrueck.tenbeitel.mr.twittergraph.mapper;
 
+import java.io.IOException;
+
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -8,5 +10,12 @@ import org.apache.mahout.clustering.classify.WeightedPropertyVectorWritable;
 import de.hs.osnabrueck.tenbeitel.mr.twittergraph.io.TwitterWritable;
 
 public class ClusterDataJoinMapper extends Mapper<IntWritable, WeightedPropertyVectorWritable, Text, TwitterWritable>{
+
+	@Override
+	protected void map(IntWritable key, WeightedPropertyVectorWritable value,
+			Mapper<IntWritable, WeightedPropertyVectorWritable, Text, TwitterWritable>.Context context)
+					throws IOException, InterruptedException {
+		
+	}
 	
 }
