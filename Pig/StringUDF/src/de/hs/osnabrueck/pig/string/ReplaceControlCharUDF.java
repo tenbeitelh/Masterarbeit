0f7a75be88cec0a7ac3070c4f2be97dcd34cbe95
tenbeitelh <tenbeitelholger@googlemail.com>
2016-01-19 07:54:12 +0100

    replace control characters

---
+++ Pig/StringUDF/src/de/hs/osnabrueck/pig/string/ReplaceControlCharUDF.java
@@ -0,0 +1,30 @@
+package de.hs.osnabrueck.pig.string;
+
+import java.io.IOException;
+
+import org.apache.pig.EvalFunc;
+import org.apache.pig.data.Tuple;
+
+import com.google.common.base.CharMatcher;
+
+public class ReplaceControlCharUDF extends EvalFunc<String> {
+
+	@Override
+	public String exec(Tuple input) throws IOException {
+		if (input == null || input.size() == 0) {
+			return null;
+		}
+		Object text = input.get(0);
+		if (text == null) {
+			return null;
+		}
+		return replaceControlCharacters((String) text);
+	}
+
+	private String replaceControlCharacters(String text) {
+		// String replaced = text.replaceAll("[\u0000-\u001f]", " ");
+		String replaced = CharMatcher.JAVA_ISO_CONTROL.replaceFrom(text, " ");
+		return replaced.replaceAll("^ +| +$|( )+", " ");
+	}
+
+}
