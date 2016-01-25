beb24e0e1c0c40175c449810a7c82bf2176e36cf
tenbeitelh <tenbeitelholger@gmx.de>
2016-01-25 19:30:05 +0100

    test

--- Graph/InitialInformationFlow/src/de/hs/osnabrueck/tenbeitel/mr/informationflow/mapper/InformationflowMapper.java
+++ Graph/InitialInformationFlow/src/de/hs/osnabrueck/tenbeitel/mr/informationflow/mapper/InformationflowMapper.java
@@ -11,7 +11,7 @@ public class InformationflowMapper extends Mapper<LongWritable, Text, Text, Text
 	private Text repliedByStatus = new Text();
 	@Override
 	protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-		String statusIds[] = value.toString().split("|");
+		String statusIds[] = value.toString().split("\\t");
 		if (statusIds.length == 2) {
 			statusId.set(statusIds[1]);
 			repliedByStatus.set(statusIds[0]);
