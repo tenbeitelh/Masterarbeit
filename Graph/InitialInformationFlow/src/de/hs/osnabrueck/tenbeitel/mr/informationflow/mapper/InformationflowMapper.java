e2e5c1affba8046cd1b4cbf45262781becfabcdd
tenbeitelh <tenbeitelholger@gmx.de>
2016-01-25 19:27:02 +0100

    changed delimiter

--- Graph/InitialInformationFlow/src/de/hs/osnabrueck/tenbeitel/mr/informationflow/mapper/InformationflowMapper.java
+++ Graph/InitialInformationFlow/src/de/hs/osnabrueck/tenbeitel/mr/informationflow/mapper/InformationflowMapper.java
@@ -11,7 +11,7 @@ public class InformationflowMapper extends Mapper<LongWritable, Text, Text, Text
 	private Text repliedByStatus = new Text();
 	@Override
 	protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-		String statusIds[] = value.toString().split("\t");
+		String statusIds[] = value.toString().split("|");
 		if (statusIds.length == 2) {
 			statusId.set(statusIds[1]);
 			repliedByStatus.set(statusIds[0]);
