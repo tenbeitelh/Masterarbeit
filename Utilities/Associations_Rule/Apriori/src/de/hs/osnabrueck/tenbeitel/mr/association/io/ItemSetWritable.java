0e4302e110a498f2d8fef92573ddc4a7820f8c34
tenbeitelh <tenbeitelholger@googlemail.com>
2016-01-28 16:20:03 +0100

    Apriori Wirtable

--- Utilities/Associations_Rule/Apriori/src/de/hs/osnabrueck/tenbeitel/mr/association/io/ItemSetWritable.java
+++ Utilities/Associations_Rule/Apriori/src/de/hs/osnabrueck/tenbeitel/mr/association/io/ItemSetWritable.java
@@ -5,7 +5,11 @@ import java.util.Arrays;
 import org.apache.hadoop.io.ArrayWritable;
 import org.apache.hadoop.io.Text;
 
+import de.hs.osnabrueck.tenbeitel.mr.association.utils.AprioriUtils;
+
 public class ItemSetWritable extends ArrayWritable {
+	private String test;
+
 	public ItemSetWritable() {
 		super(Text.class);
 	}
@@ -28,4 +32,29 @@ public class ItemSetWritable extends ArrayWritable {
 		}
 		return stringItemSet;
 	}
+
+	@Override
+	public int hashCode() {
+		Text[] objects = (Text[]) get();
+		int hash = 0;
+		for (Text object : objects) {
+			hash += object.hashCode();
+		}
+		return hash;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ItemSetWritable other = (ItemSetWritable) obj;
+		String[] thisTextObjects = getStringItemSet();
+		String[] textObjects = other.getStringItemSet();
+		return AprioriUtils.arrayContainsSubset(thisTextObjects, textObjects);
+	}
+
 }
